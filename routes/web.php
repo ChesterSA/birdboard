4968000c952b97bae37b1d11e71c1dcf260f4252
ChesterSA <c.swannauger@gmail.com>
2019-09-05 17:25:03 +0100

    Added auth checks for project management

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/projects', 'ProjectsController@store')->middleware('auth');
+Route::group(['middleware' => 'auth'], function () {
+  Route::post('/projects', 'ProjectsController@store');
+  Route::get('/projects', 'ProjectsController@index');
+  Route::get('/projects/{project}', 'ProjectsController@show');
 
-Route::get('/projects', 'ProjectsController@index');
+  Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/projects/{project}', 'ProjectsController@show');
+});
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
